f51c5b607061ef374f69b74b654f1d1136e97c2c
simus <sirojiddinmustafayev@gmail.com>
2021-08-04 19:28:38 +0500

    poetry, django-environ, django 3.2.6 configured, code improving

--- pages/models.py
+++ pages/models.py
@@ -1,10 +1,8 @@
 from django.db import models
-from ckeditor.fields import RichTextField
 from ckeditor_uploader.fields import RichTextUploadingField
-from django.utils.text import slugify
 from mptt.models import MPTTModel, TreeForeignKey
 
-# Create your models here.
+
 class HelpPages(MPTTModel):
     title = models.CharField(max_length=255)
     content = RichTextUploadingField(blank=True, null=True)
@@ -15,17 +13,10 @@ class HelpPages(MPTTModel):
     date = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
-    # class Meta(object):
-    #     ordering = ['position']
-
     class MPTTMeta:
         parent_attr = 'parent_page'
         # tree_id_attr = 'position'
         # order_insertion_by = ['position']
-
-    # def save(self, *args, **kwargs):
-    #     self.slug = slugify(self.title)
-    #     super(HelpPages, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.title
